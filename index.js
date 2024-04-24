7de966f9f1a50c15bdad4560c56ef29e833f55f1
youness <youness.benakkaf@coda-student.school>
2024-04-24 19:08:26 +0200

    modif cv

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function onPageLinkClicked(page) {
 function downloadCV() {
     
     const link = document.createElement('a');
-    link.href = '../assets/cvybk.pdf'; 
+    link.href = '../assets/cvyk.pdf'; 
     link.download = 'cv.pdf'; 
     document.body.appendChild(link);
     link.click(); 
